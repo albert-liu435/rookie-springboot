c0f2e609e49f65e35789ee775a67565d1d87d4c1
rookie <415242678@qq.com>
2021-06-18 11:36:28 +0800

    Springboot与Cors解决跨域问题2

---
+++ rookie-springbootweb/rookie-springboot-cors/src/main/java/com/rookie/bigdata/config/FilterCorsConfig.java
@@ -0,0 +1,39 @@
+package com.rookie.bigdata.config;
+
+import com.rookie.bigdata.config.properties.CorsProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @ClassName FilterCorsConfig
+ * @Description FilterCorsConfig
+ * @Author rookie
+ * @Date 2021/6/18 11:32
+ * @Version 1.0
+ */
+@Configuration
+public class FilterCorsConfig {
+    @Bean
+    public FilterRegistrationBean corsFilter(CorsProperties corsProperties) {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 跨域配置
+        corsConfiguration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        corsConfiguration.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        corsConfiguration.setAllowedMethods(corsProperties.getAllowedMethods());
+        corsConfiguration.setAllowCredentials(corsProperties.isAllowCredentials());
+        corsConfiguration.setMaxAge(corsConfiguration.getMaxAge());
+
+        source.registerCorsConfiguration(corsProperties.getPath(), corsConfiguration);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        bean.setEnabled(corsProperties.isEnable());
+        return bean;
+    }
+}
