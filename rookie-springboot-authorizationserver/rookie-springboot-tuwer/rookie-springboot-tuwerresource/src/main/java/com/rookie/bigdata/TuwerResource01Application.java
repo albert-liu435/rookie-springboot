002336a49b01f64547873f4cfe3fb564bed73c96
liuxili01 <liuxili@bestseller.com.cn>
2024-08-13 18:51:44 +0800

    rookie-springboot-tuwer TuwerResource01Application

--- rookie-springboot-authorizationserver/rookie-springboot-tuwer/rookie-springboot-tuwerresource/src/main/java/com/rookie/bigdata/TuwerResource01Application.java
+++ rookie-springboot-authorizationserver/rookie-springboot-tuwer/rookie-springboot-tuwerresource/src/main/java/com/rookie/bigdata/TuwerResource01Application.java
@@ -4,17 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @ClassName TuwerResourceApplication
- * @Description TuwerResourceApplication
+ * @ClassName TuwerResource01Application
+ * @Description TuwerResource01Application
  * @Author rookie
  * @Date 2021/6/18 10:14
  * @Version 1.0
  */
 @SpringBootApplication
-public class TuwerResourceApplication {
+public class TuwerResource01Application {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(TuwerResourceApplication.class);
+        SpringApplication springApplication = new SpringApplication(TuwerResource01Application.class);
         springApplication.run(args);
     }
 
